b6bd01aec0abe5902ffbca590e48ac349779f30c
MY PC <tofikvora6@gmail.com>
2025-07-24 22:49:32 +0530

    About Section Done

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,63 @@
-import React from 'react'
+import React from "react";
+import Tilt from "react-tilt";
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { services } from "../Const";
+import { fadeIn, textVariant } from "../Utils/motion";
+import { SectionWraper } from "../hoc";
+
+const ServiceCard = ({ index, title, icon }) => {
+  return (
+    <Tilt className="xs:w-[250px] w-full">
+      <motion.div
+        variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
+        className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
+      >
+        <div
+          options={{
+            max: 45,
+            scale: 1,
+            speed: 450,
+          }}
+          className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
+        >
+          <img src={icon} alt={title} className="w-16 h-16 object-contain" />
+          <h3 className="text-white text-[20px] font-bold text-center">{title}</h3>
+        </div>
+      </motion.div>
+    </Tilt>
+  );
+};
+
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>OverView.</h2>
+      </motion.div>
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
+      >
+        I'm a skilled full-stack developer with hands-on experience in Flutter
+        for mobile app development and .NET Core with MVC architecture for
+        robust backend solutions. I specialize in building scalable, secure, and
+        high-performance applications using MS SQL Server for reliable data
+        management. I'm passionate about writing clean, maintainable code and
+        collaborating with clients to deliver intuitive, user-friendly solutions
+        that solve real-world problems. Let’s work together to turn your vision
+        into a powerful digital product!
+      </motion.p>
+      <div className="mt-20 flex flex-wrap gap-10">
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </>
+  );
+};
 
-export default About
+export default SectionWraper(About,"about");
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,10 +1,10 @@
 import { Suspense, useEffect, useState } from 'react';
-import { Canvas } from '@react-three/fiber';
+import { Canvas, events } from '@react-three/fiber';
 import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
 import CanvasLoader from '../Loader';
 
 // 3D Model Component
-const ComputerModel = () => {
+const ComputerModel = ({isMobile}) => {
   const computer = useGLTF('./public/desktop_pc/scene.gltf');
 
   return (
@@ -20,9 +20,9 @@ const ComputerModel = () => {
       
       />
       <primitive object={computer.scene}
-      scale={0.75}
+      scale={isMobile ? 0.7 :0.75}
 
-      position={[0,-3.5,-1.5]}
+      position={isMobile ? [0,-3,-2.2]:[0,-3.5,-1.5]}
       rotation={[-0.01,-0.2,-0.1]}
       />
     </mesh>
@@ -31,6 +31,21 @@ const ComputerModel = () => {
 
 // Canvas Component
 const ComputersCanvas = () => {
+
+  const [isMobile,setIsMobile] = useState(false);
+
+  useEffect(() =>{
+    const MediaQuery = window.matchMedia('(max-width:500px)');
+    setIsMobile(MediaQuery.matches);
+
+    const handleMediaQueryChange = (events) =>{
+      setIsMobile(events.matches);
+    }
+    MediaQuery.addEventListener('change',handleMediaQueryChange);
+    return () =>{
+      MediaQuery.removeEventListener('change',handleMediaQueryChange);
+    }
+  });
   return (
     <Canvas
       frameloop="demand"
@@ -44,7 +59,7 @@ const ComputersCanvas = () => {
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
         />
-        <ComputerModel />
+        <ComputerModel isMobile={isMobile}/>
         <Preload all />
       </Suspense>
     </Canvas>
